d9c666aacfb370570bc6cb5c835a89249a6e5c7c
javier-pardinas <javierpb275@gmail.com>
2021-04-14 10:14:23 +0200

    configured plugins

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//PLUGINS
+requestAnimationFrame('./plugins');
+
 Vue.config.productionTip = false
 
 new Vue({
