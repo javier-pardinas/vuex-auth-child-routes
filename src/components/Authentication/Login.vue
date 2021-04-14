817adae1cfe7cab0dd06e17e2e33288b1a7f67a7
javier-pardinas <javierpb275@gmail.com>
2021-04-14 14:44:53 +0200

    created login component in authentication folder in components

---
+++ src/components/Authentication/Login.vue
@@ -0,0 +1,22 @@
+<template>
+    
+</template>
+
+<script>
+
+export default {
+    props: {
+        user: {
+            type: Object,
+            required: true ,
+            validator: user => {
+                if (!user.hasOwnProperty('email') || !user.hasOwnProperty('password')) {
+                    console.warn('Not valid User');
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
+</script>
